24ecff5ac6b70a4cc830eb9d601bc6e91ca8c3a3
Dean Chuang <azurenokite@gmail.com>
2016-05-08 00:36:22 +0000

    add active class for links

--- app/app.jsx
+++ app/app.jsx
@@ -22,7 +22,7 @@ ReactDOM.render(
 
 //1 load in everything from the react router module using es6 syntax
 //2 set the route in the ReactDOM.render and then start making individual routes
-//3 create our root path to route to the Main component
+//3 create our root path to route to the Main component (using {} to pass in the javascript expression)
 //4 add the alias to our webpack configuration
 //5 create the Nav component and load it to Main
 //6 create a Weather component and load it to the main app.jsx for routing
@@ -31,3 +31,4 @@ ReactDOM.render(
 //9 make an about component to render the about page
 //10 create route for the About component using Route and it will also be rendered as this.props.children in the Main component
 //11 create links by importing the Link function via the react-router module inside of the Nav component
+//12 add activeClassName to the links. change the link for Weather to IndexLink(need to pull from react-router) because it is matched with an IndexRoute
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,14 +1,14 @@
 var React = require('react');
-var {Link} = require('react-router');
+var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
   render: function(){
     return (
       <nav>
         <h2>I am a nav</h2>
-        <Link to="/">Current Weather</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/example">Example</Link>
+        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Current Weather</IndexLink>
+        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About Us</Link>
+        <Link to="/example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link>
       </nav>
     );
   }
