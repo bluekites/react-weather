1e1bd029c8597251a3944dac32220ad92b5701a4
Dean Chuang <azurenokite@gmail.com>
2016-05-11 04:58:44 +0000

    update error modal with property for dynamic message

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,16 +1,26 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+  getDefaultProps: function(){
+    return ({
+      title: 'Error!'
+    });
+  },
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
+  },
   componentDidMount: function(){  // componentDidMount will be trigger once the component has been rendered
     // create a new instance of foundation.reveal
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   },
   render: function(){
+    var {title, message} = this.props;
     return (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-        <h4>Error Detected</h4>
-        <p>Could not find city!</p>
+        <h4>{title}</h4>
+        <p>{message}</p>
         <div>
           <button className="button hollow" data-close="">Okay</button> {/* data-close lets us close the modal upon click */}
         </div>
